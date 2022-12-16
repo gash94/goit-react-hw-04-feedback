0889108a899389695e4922f5b2dd94e771f01f79
gash94 <104354207+gash94@users.noreply.github.com>
2022-12-16 16:28:19 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,66 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistisc';
+import Notification from './Notification/Notification';
+import css from './App.module.css';
 
-const App = () => {
-  return <>React homework template</>;
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    const value = Math.round((good / total) * 100);
+    return !Number.isNaN(value) ? value : 0;
+  };
+  formatValue = value => {
+    return `${Number(value).toFixed(0)}`;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    return (
+      <div className={css.app}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+              formatValue={this.formatValue}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
 
 export default App;
